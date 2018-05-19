fe34bf118173c358a4db8b1f39d22e2647574e39
ducdung98tb <dungleduc03081998@gmail.com>
2018-05-19 11:16:44 +0700

    a

--- learnGA/example.py
+++ learnGA/example.py
@@ -1,38 +1,56 @@
-# DEAP
-import numpy as np
+geneSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+target = "Hello World"
+
 import random
+import datetime
+import matplotlib.pyplot as plt
+def initialize_chromosomes(length):
+    chromosomes = []
+    while len(chromosomes) < length:
+        sampleSize = min(length - len(chromosomes), len(geneSet))
+        chromosomes.extend(random.sample(geneSet, sampleSize))
+    return ''.join(chromosomes)
+def error_function(chromosome):
+    return len(target) - sum(1 for expected, actual \
+                             in zip(target, chromosome)  \
+                             if expected == actual)
+def mutate(parent):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet, 2)
+    childGenes[index] = alternate \
+    if newGene == childGenes[index] \
+    else newGene
+    return ''.join(childGenes)
 
-from deap import base
-from deap import creator
-from deap import tools
-creator.create('FitnessMax', base.Fitness, weights=(1, ))
-creator.create('Individual', np.ndarray, fitness=creator.FitnessMax)
-toolbox = base.Toolbox()
-toolbox.register('attr_float', np.random.uniform)
-toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.attr_float, genom_dimension())
-toolbox.register('population', tools.initRepeat, list, toolbox.individual)
-toolbox.register('evaluate', evaluate)
-toolbox.register('mate', tools.cxSimulatedBinaryBounded, eta=100, low=0, up=1)
-toolbox.register('mutate', tools.mutPolynomialBounded, eta=100, low=0, up=1, indpb=0.01)
-toolbox.register('select', tools.selTournament, tournsize=3)
+def display(guess):
+    timeDiff = (datetime.datetime.now() - startTime)
+    fitness = error_function(guess)
+    print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
+    return fitness, timeDiff
+error = []
+times = []
+random.seed(1)
+startTime = datetime.datetime.now()
+bestParent = initialize_chromosomes(len(target))
+bestFitness = error_function(bestParent)
+display(bestParent)
+while True:
+    child = mutate(bestParent)
+    childFitness = error_function(child)
 
-def main():
-    os.system('rm population/*')
+    if bestFitness < childFitness:
+        continue
+    fitness, timeDiff =  display(child)
+    error.append(fitness)
+    times.append(str(timeDiff))
+    if childFitness == 0:
+        break
+    bestFitness = childFitness
+    bestParent = child
 
-    np.random.seed(1337)
-    
-    pop = toolbox.population(n=50)
-    hof = tools.HallOfFame(1, similar=np.array_equal)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register('avg', np.mean)
-    stats.register('std', np.std)
-    stats.register('min', np.min)
-    stats.register('max', np.max)
-
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.1, ngen=40, 
-                                       stats=stats, halloffame=hof, verbose=True)
-    total_nevals = sum(log.select('nevals'))
-    np.save('population/cartpole-v1-hof.npy', hof[0])
-    np.save('population/cartpole-v1-ind.npy', pop[0])
-
-    return pop, log, hof
+plt.plot(times, error)
+plt.title("Error")
+plt.xlabel("time")
+plt.ylabel("error")
+plt.show()
